a9b69ba30fbee3c61fd4e3f430cc284469c85ff3
yehmight <145411012+yehmight@users.noreply.github.com>
2025-06-25 01:28:39 +0100

    Update FooterContact.tsx

--- src/app/components/FooterContact.tsx
+++ src/app/components/FooterContact.tsx
@@ -79,7 +79,7 @@ export default function FooterContact() {
         {/* Right - Contact Info */}
 <div className="flex flex-col mt-20 justify-between">
   <div>
-    <h3 className="text-red-400 uppercase text-sm tracking-wide mb-2">Let's Talk</h3>
+    <h3 className="text-red-400 uppercase text-sm tracking-wide mb-2">Let Us Talk</h3>
     <h2 className="text-3xl font-bold mb-4">Keep In Touch</h2>
     <p className="text-gray-300 mb-6">
       Whether you have a question, want to work together, or just want to say hi — I will try my best to get back to you!
